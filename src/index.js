89a83d6bcdbe04ce85080499303db4d71f99b77c
howardmann <howardmann27@gmail.com>
2017-08-23 22:44:40 +1000

    Add public js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+let API = require('./util/api.js');
+let axios = require('axios');
+
+$(document).ready(function(){
+  // cache DOM
+  let $main = $('#main');
+
+  // Fetch stock
+  API.fetchCompany(axios, 'aapl')
+    .then(response => API.parse(response.data))
+    .then(output => $main.html(output))
+})
