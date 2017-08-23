ee2ee9f17a451d4a86456147f3379d6b56509ad5
howardmann <howardmann27@gmail.com>
2017-08-23 22:56:17 +1000

    Restructure folders for production

--- src/index.js
+++ src/index.js
@@ -4,9 +4,21 @@ let axios = require('axios');
 $(document).ready(function(){
   // cache DOM
   let $main = $('#main');
+  let $form = $('#form-search');
+  let $input = $('#input-search');
+
+  // Event handler listen for search submit
+  $form.on('submit', function(e){
+    e.preventDefault();
+    let ticker = $input.val();
+    fetchTicker(ticker);
+  })
 
   // Fetch stock
-  API.fetchCompany(axios, 'aapl')
-    .then(response => API.parse(response.data))
-    .then(output => $main.html(output))
+  function fetchTicker(ticker){
+    API.fetchCompany(axios, ticker)
+      .then(response => API.parse(response.data))
+      .then(output => $main.html(output))
+      .catch(err => $main.html(err))
+  }
 })
